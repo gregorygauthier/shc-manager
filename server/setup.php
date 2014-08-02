2cfbcb422530873052f2da7e7b990460f1ee0cbb
Gregory Gauthier <rjaguar3netgames@yahoo.com>
2014-08-02 00:17:22 -0400

    Rewrite database to use InnoDB, utf8 and optimize.
    InnoDB is now used by all tables for better handling (including
    foreign key support).  utf8 is also good to have.
    
    The other major change is that the grades table is no more.
    Instead, the grade is now stored as part of the player_response.
    Also, the grades are calculated smartly:
    * when a player_response is inserted or updated, it is graded
    * when a response to a clue is inserted, updated or deleted,
      all player_responses for that clue are graded.
    This should fix a major performance issue in previous years, where
    the grades table, consisting of tens of thousands of player_responses
    matched by regex against numerous clue responses, took significant
    time to generate.

--- server/setup.php
+++ server/setup.php
@@ -32,22 +32,33 @@ require_once('common.inc');?>
 <?php
 $mysqli = connect_mysql();
 
-/*echo "The database name is $mysql_dbname.";
+echo "The database name is $mysql_dbname.";
 
 $query = "CREATE DATABASE $mysql_dbname;";
 
 $mysqli->query($query) or die('<html>Database already created.  If the
-database does not work, please delete it manually and run this script again.</html>');*/
+database does not work, please delete it manually and run this script again.</html>');
 
 $query = "USE $mysql_dbname;";
 
-$mysqli->query($query);
+$mysqli->query($query) or die(sprintf("Error executing query $query: %s", $mysqli->error));
 
 $query = "CREATE TABLE rounds (
   id SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30),
   sequence SMALLINT NOT NULL,
-  is_regular BOOLEAN NOT NULL);";
+  is_regular BOOLEAN NOT NULL)
+  ENGINE = InnoDB,
+  DEFAULT CHARSET = utf8;";
+
+$mysqli->query($query) or die(sprintf("Error executing query $query: %s", $mysqli->error));
+
+$query = "CREATE TABLE toc_berths (
+  id TINYINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  abbreviation VARCHAR(20),
+  description TEXT)
+  ENGINE = InnoDB,
+  DEFAULT CHARSET = utf8;";
 
 $mysqli->query($query);
 
@@ -60,9 +71,11 @@ $query = "CREATE TABLE days (
   thread_url VARCHAR(255),
   highest_parsed_post SMALLINT DEFAULT -1,
   FOREIGN KEY (round_id) REFERENCES rounds(id),
-  INDEX round_id_idx (round_id));";
+  INDEX round_id_idx (round_id))
+  ENGINE = InnoDB,
+  DEFAULT CHARSET = utf8;";
 
-$mysqli->query($query);
+$mysqli->query($query) or die(sprintf("Error executing query $query: %s", $mysqli->error));
 
 $query = "CREATE TABLE categories (
   id SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -71,9 +84,11 @@ $query = "CREATE TABLE categories (
   day_id SMALLINT,
   sequence SMALLINT NOT NULL,
   FOREIGN KEY (day_id) REFERENCES days(id),
-  INDEX day_id_idx (day_id));";
+  INDEX day_id_idx (day_id))
+  ENGINE = InnoDB,
+  DEFAULT CHARSET = utf8;";
 
-$mysqli->query($query);
+$mysqli->query($query) or die(sprintf("Error executing query $query: %s", $mysqli->error));
 
 $query = "CREATE TABLE clues (
   id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -82,9 +97,11 @@ $query = "CREATE TABLE clues (
   point_value SMALLINT,
   wrong_point_value SMALLINT,
   FOREIGN KEY (category_id) REFERENCES categories(id),
-  INDEX category_id_idx (category_id));";
+  INDEX category_id_idx (category_id))
+  ENGINE = InnoDB,
+  DEFAULT CHARSET = utf8;";
 
-$mysqli->query($query);
+$mysqli->query($query) or die(sprintf("Error executing query $query: %s", $mysqli->error));
 
 $query = "CREATE TABLE responses (
   id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -92,9 +109,11 @@ $query = "CREATE TABLE responses (
   response_text TEXT,
   correct BOOLEAN,
   FOREIGN KEY (clue_id) REFERENCES clues(id),
-  INDEX clue_id_idx (clue_id));";
+  INDEX clue_id_idx (clue_id))
+  ENGINE = InnoDB,
+  DEFAULT CHARSET = utf8;";
 
-$mysqli->query($query);
+$mysqli->query($query) or die(sprintf("Error executing query $query: %s", $mysqli->error));
 
 $query = "CREATE TABLE players (
   id SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -105,79 +124,152 @@ $query = "CREATE TABLE players (
   rookie_eligible BOOLEAN,
   toc TINYINT,
   FOREIGN KEY (toc) REFERENCES toc_berths(id),
-  INDEX username_idx (username));";
+  INDEX username_idx (username))
+  ENGINE = InnoDB,
+  DEFAULT CHARSET = utf8;";
 
-$mysqli->query($query);
+$mysqli->query($query) or die(sprintf("Error executing query $query: %s", $mysqli->error));
 
 $query = "CREATE TABLE player_responses (
   id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   player_id SMALLINT,
   clue_id MEDIUMINT,
   response_text TEXT,
+  grade BOOLEAN,
   FOREIGN KEY (player_id) REFERENCES players(id),
   FOREIGN KEY (clue_id) REFERENCES clues(id),
   INDEX player_id_idx (player_id),
   INDEX clue_id_idx (clue_id),
-  UNIQUE player_and_clue_idx (player_id, clue_id));";
+  UNIQUE player_and_clue_idx (player_id, clue_id))
+  ENGINE = InnoDB,
+  DEFAULT CHARSET = utf8;";
 
-$mysqli->query($query);
-
-$query = "CREATE TABLE toc_berths (
-  id TINYINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  abbreviation VARCHAR(20),
-  description TEXT);";
-
-$mysqli->query($query);
+$mysqli->query($query) or die(sprintf("Error executing query $query: %s", $mysqli->error));
 
 $query = "CREATE TABLE users (
   id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255),
   hashed_password CHAR(128),
   email VARCHAR(255),
-  INDEX username_idx (username));";
+  INDEX username_idx (username))
+  ENGINE = InnoDB,
+  DEFAULT CHARSET = utf8;";
 
-$mysqli->query($query);
+$mysqli->query($query) or die(sprintf("Error executing query $query: %s", $mysqli->error));
 
-$query = "CREATE VIEW grades AS SELECT
-  player_id, pr.clue_id, MAX(correct) AS grade FROM player_responses AS pr
-  LEFT JOIN responses AS r on pr.clue_id = r.clue_id AND
-  pr.response_text REGEXP CONCAT('(^', r.response_text, '$)')
-  WHERE pr.response_text IS NOT NULL
-  AND pr.response_text <> '' GROUP BY pr.clue_id, player_id;";
+$query = "CREATE FUNCTION grade_response(clue_id MEDIUMINT, ungraded_response_text TEXT)
+RETURNS BOOLEAN
+LANGUAGE SQL
+COMMENT 'Grade the response against clue_id'
+BEGIN
+    DECLARE candidate_response TEXT;
+    DECLARE done BOOLEAN;
+    DECLARE grade BOOLEAN;
+    DECLARE matches_incorrect BOOLEAN;
+    DECLARE response_cur CURSOR FOR
+        SELECT response_text, correct FROM responses AS r
+        WHERE r.clue_id=clue_id;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    
+    SET done = FALSE;
+    SET matches_incorrect = FALSE;
+    OPEN response_cur;
+    
+    grade_loop: LOOP
+        FETCH response_cur INTO candidate_response, grade;
+        IF done THEN
+            LEAVE grade_loop;
+        END IF;
+        IF ungraded_response_text REGEXP candidate_response THEN
+            IF grade THEN
+                CLOSE response_cur;
+                RETURN TRUE;
+            ELSE SET matches_incorrect = TRUE;
+            END IF;
+        END IF;
+    END LOOP;
+    
+    CLOSE response_cur;
+    IF matches_incorrect THEN
+        RETURN FALSE;
+    ELSE
+        RETURN NULL;
+    END IF;
+END;";
 
-$mysqli->query($query);
+$mysqli->query($query) or die(sprintf("Error executing query $query: %s", $mysqli->error));
+
+$query = "CREATE PROCEDURE grade_all_responses(IN clue_id MEDIUMINT)
+BEGIN
+    UPDATE player_responses
+        SET grade=grade_response(player_responses.clue_id, response_text)
+    WHERE player_responses.clue_id = clue_id;
+END;";
+
+$mysqli->query($query) or die(sprintf("Error executing query $query: %s", $mysqli->error));
+
+$query = "CREATE TRIGGER bi_grade_response BEFORE INSERT ON player_responses
+FOR EACH ROW SET NEW.grade = grade_response(NEW.clue_id, NEW.response_text);";
+
+$mysqli->query($query) or die(sprintf("Error executing query $query: %s", $mysqli->error));
+
+$query = "CREATE TRIGGER bu_grade_response BEFORE UPDATE ON player_responses
+FOR EACH ROW SET NEW.grade = grade_response(NEW.clue_id, NEW.response_text);";
+
+$mysqli->query($query) or die(sprintf("Error executing query $query: %s", $mysqli->error));
+
+$query = "CREATE TRIGGER ai_grade_all_responses AFTER INSERT ON responses
+FOR EACH ROW CALL grade_all_responses(NEW.clue_id);";
+
+$mysqli->query($query) or die(sprintf("Error executing query $query: %s", $mysqli->error));
+
+$query = "CREATE TRIGGER au_grade_all_responses AFTER UPDATE ON responses
+FOR EACH ROW
+BEGIN
+    CALL grade_all_responses(OLD.clue_id);
+    IF NEW.clue_id != OLD.clue_id THEN
+        CALL grade_all_responses(NEW.clue_id);
+    END IF;
+END;";
+
+$mysqli->query($query) or die(sprintf("Error executing query $query: %s", $mysqli->error));
+
+$query = "CREATE TRIGGER ad_grade_all_responses AFTER DELETE ON responses
+FOR EACH ROW CALL grade_all_responses(OLD.clue_id);";
+
+$mysqli->query($query) or die(sprintf("Error executing query $query: %s", $mysqli->error));
 
 $query = "CREATE VIEW scores AS SELECT player_id, clue_id,
   categories.id AS category_id, days.id AS day_id, rounds.id AS round_id,
   IF(grade IS NULL, 0, IF(grade=1, point_value, wrong_point_value)) AS score,
-  IF(grade IS NULL, 1, 0) AS ungraded FROM grades
+  IF(grade IS NULL, 1, 0) AS ungraded FROM player_responses
   INNER JOIN clues ON clue_id = clues.id
   LEFT JOIN categories ON clues.category_id = categories.id
   LEFT JOIN days ON categories.day_id = days.id LEFT JOIN
   rounds ON days.round_id = rounds.id;";
 
-$mysqli->query($query);
+$mysqli->query($query) or die(sprintf("Error executing query $query: %s", $mysqli->error));
 
 $query = "CREATE VIEW category_scores AS
   SELECT player_id, category_id, day_id, round_id, SUM(score) AS category_score,
   SUM(ungraded) AS category_ungraded
   FROM scores GROUP BY player_id, category_id;";
 
-$mysqli->query($query);
+$mysqli->query($query) or die(sprintf("Error executing query $query: %s", $mysqli->error));
 
 $query = "CREATE VIEW daily_scores AS
   SELECT player_id, day_id, round_id, SUM(score) AS daily_score,
   SUM(ungraded) AS daily_ungraded FROM scores
   GROUP BY player_id, day_id;";
 
-$mysqli->query($query);
+$mysqli->query($query) or die(sprintf("Error executing query $query: %s", $mysqli->error));
 
 $query = "CREATE VIEW round_scores AS
   SELECT player_id, round_id, SUM(score) AS round_score,
   SUM(ungraded) AS round_ungraded FROM scores
   GROUP BY player_id, round_id;";
 
-$mysqli->query($query);
+$mysqli->query($query) or die(sprintf("Error executing query $query: %s", $mysqli->error));
 
 $query = "CREATE VIEW overall_scores AS
   SELECT player_id, SUM(score) AS overall_score,
@@ -186,7 +278,9 @@ $query = "CREATE VIEW overall_scores AS
   WHERE rounds.is_regular = 1
   GROUP BY player_id;";
 
-$mysqli->query($query);
+$mysqli->query($query) or die(sprintf("Error executing query $query: %s", $mysqli->error));
+
+
 
 echo("<!DOCTYPE html>
 <HTML><HEAD></HEAD><BODY><h1>Database successfully created!
