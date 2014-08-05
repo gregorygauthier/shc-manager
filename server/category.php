f2df46606073896f4f49ee07512e4fe5c09d3415
Gregory Gauthier <rjaguar3netgames@yahoo.com>
2014-08-05 10:04:14 -0400

    Highlight ungraded counts.
    Another minor change is that ungraded in the standings table
    now uses the count class rather than the score class.

--- server/category.php
+++ server/category.php
@@ -173,7 +173,7 @@ if(!isset($errortext))
                 $clue->clue_statistics->num_clam);
             if ($clue->clue_statistics->num_ungraded)
             {
-                printf ('<td class="count">%d</td>',
+                printf ('<td class="alert count">%d</td>',
                     $clue->clue_statistics->num_clam);
             }
             else
