61c3f1e902079b2cad7192bc752c77779e95c5e3
Gregory Gauthier <rjaguar3netgames@yahoo.com>
2014-08-06 11:07:50 -0400

    Repurpose autofill.php to be an interfact to autofill_cron.

--- server/autofill.php
+++ server/autofill.php
@@ -22,37 +22,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 require_once('common.inc');
-startpage(RESTRICTED);
-
-do
-{
-    $mysqli = connect_mysql();
-    if(!$mysqli)
-    {
-        $errortext = "Could not connect to database.";
-        break;
-    }
-    $mysqli->query("USE $mysql_dbname;");
-    $query = "SELECT days.id, days.name FROM days LEFT JOIN rounds
-    ON days.round_id=rounds.id
-    ORDER BY rounds.sequence ASC, days.sequence ASC";
-    $stmt = $mysqli->prepare($query);
-    if(!$stmt)
-    {
-        $errortext = "Could not prepare statement";
-        $mysqli->close();
-        break;
-    }
-    $stmt->execute();
-    $stmt->bind_result($id, $name);
-    $days = array();
-    while($stmt->fetch())
-    {
-        $days[$id] = $name;
-    }
-    $stmt->close();
-    $mysqli->close();
-} while (false)
+startpage(ADMIN);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -60,49 +30,16 @@ do
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
 <link rel="stylesheet" type="text/css" href="theme.css" />
 <link rel="icon" type="image/png" href="shcicon.png" />
-<title>
-<?php
-if(isset($errortext))
-{
-    echo 'Error preparing autofill page.';
-}
-else
-{
-    echo 'Autofill';
-}
-?>
-</title>
+<title>Autofill</title>
 </head>
 <body>
+<h1>Autofill</h1>
+<p>Click the button below to run the autofill script.</p>
+<form action="autofill_cron.php" method="GET">
+<button onsubmit="this.disabled=true">Run autofill script</button>
+</form>
 <?php
-if(isset($errortext))
-{
-    echo '<h1>Error preparing autofill</h1>';
-    displayError($errortext);
-}
-else
-{
-    echo '<hgroup><h1>Autofill</h1>';
-    echo '<h2>With great power comes great responsibility: ';
-    echo 'please be careful!</h2></hgroup>';
-    echo '<form>';
-    echo '<select id="dayselector" name="dayid">';
-    echo '<option value="0">Please select a day...</option>';
-    foreach($days as $id => $name)
-    {
-        printf('<option value="%d">%s</option>', $id, $name);
-    }
-    echo '</select>';
-    echo '<label for="urlfield">URL of daily thread:</label>';
-    echo '<input id="urlfield" name="url" maxlength="255" />';
-    echo '<label for="urlfield">Post number:</label>';
-    echo '<button id="postnumberdecrement" type="button"'.
-        'onclick="decrement_post_number()">-</button>';
-    echo '<input id="postnumberfield" name="postnumber" maxlength="4" />';
-    echo '<button id="postnumberincrement" type="button"'.
-        'onclick="increment_post_number()">+</button>';
-    echo '</form>';
-}
+footer();
 ?>
 </body>
 </html>
